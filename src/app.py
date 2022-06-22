3d91dc3de71dabb4896c79fca49866952ee7c9fa
iNelan <clpayala@gmail.com>
2022-06-22 19:20:50 +0000

    version5

--- src/app.py
+++ src/app.py
@@ -1,27 +1,32 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flask import request
+import json
 app = Flask(__name__)
-
-
-
-@app.route('/todos', methods=['GET'])
-# def hello_world():
-#     return "<h1>Hello!</h1>"
-
-def hello_world():
-    # supongamos que tienes some_data (cierta información) en una variable json
-    todos = { "name": "Bobby", "lastname": "Rixer" }
-
-    # puedes convertir esa variable en un string json así
-    json_text = flask.jsonify(some_data)
-
-    # y luego puedes retornarla (return) en el response body así:
-    return json_text
 
 
 todos = [
     { "label": "My first task", "done": False },
     { "label": "My second task", "done": False }
 ]
+
+
+
+@app.route('/todos', methods=['GET'])
+def hello_world():
+    json_text = jsonify(todos)
+    return json_text
+
+# segundo endpoint
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    # request_body = request.data
+    request_body = json.loads(request.data)
+    todos.append(request_body)
+    print("Incoming request with the following body", request_body)
+    print(todos)
+    return jsonify(todos)
+
 
 
 
