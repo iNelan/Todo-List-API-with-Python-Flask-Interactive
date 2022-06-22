443adfbc702a5e6baabfe9267344abbac89b7eb0
iNelan <clpayala@gmail.com>
2022-06-22 19:36:07 +0000

    version5

--- src/app.py
+++ src/app.py
@@ -28,6 +28,14 @@ def add_new_todo():
     return jsonify(todos)
 
 
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    return 'something'
+
+
+
+
 
 
 
